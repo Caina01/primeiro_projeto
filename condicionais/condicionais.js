3aa4aa9d5cb9a99a06ddfcd6b9e1aab5b0c0e8b9
Caina01 <costacaina12@gmail.com>
2022-04-12 21:56:54 -0300

    adicionando node_modules e condicionais.

---
+++ condicionais/condicionais.js
@@ -0,0 +1,30 @@
+let casamento = "Se Deus estiver";
+let vontade = "Vontade dEle"
+//if (casamento == "Se Deus estiver" && vontade == "Vontade dEle"){
+  //  console.log("Vai dar tudo certo")
+//}else{
+ //   console.log("Vai ser pelas minhas forças")
+//}
+//function saudacao(nome, sobrenome){
+   // return 'Oi' + nome + ' ' + sobrenome;
+//  }
+//saudacao('Cainã', 'Costa')
+//console.log('Oi' + nome + ' ' + sobrenome)
+
+let dia = "segunda- feira"
+switch(dia){
+  case 'sexta-feira':
+    console.log('Bom fim de semana!')
+  case 'segunda-feira':
+  console.log('Boa semana!')
+  break;
+  default: 
+  console.log('Bom dia!');
+  
+}
+
+    for (let  i = 0; i <= 5; i++){
+    console.log("Amo eElza Luiza")
+    
+    }
+  
